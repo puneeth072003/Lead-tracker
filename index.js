6e17cc40dd9c1a52add9c26aea145c0231215f24
Puneeth <119479391+puneeth072003@users.noreply.github.com>
2023-06-16 06:02:54 +0530

    Chore(#3):Adding new tab and delete all feature

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const btnEl=document.getElementById("btn-el")
-const ipEl=document.getElementById("input-el");
+const ipEl=document.getElementById("input-el")
 let txtEl=document.getElementById("txt-el")
+let delEl=document.getElementById("del-el")
+let savEl=document.getElementById("sav-el")
 let myLeads=[]
+
+const lls=(JSON.parse(localStorage.getItem("lead")))
+if(lls){
+    myLeads=lls
+    renderlst(myLeads)
+}
+
+savEl.addEventListener("click",function(){
+    chrome.tabs.query({active:true, currentWindow: true}, function(){
+        let lnk=tabs[0].url
+        myLeads.push(lnk)
+        myL= JSON.stringify(myLeads)
+        localStorage.setItem("lead",myL)
+        renderlst(myLeads)
+        
+    })
+})
+
+delEl.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    renderlst(myLeads)
+})
 
 btnEl.addEventListener("click",function(){
    myLeads.push(ipEl.value)
    ipEl.value=""
-   renderlst()
+   myL= JSON.stringify(myLeads)
+   localStorage.setItem("lead",myL)
+   renderlst(myLeads)
 })
 
 
-function renderlst(){
+function renderlst(arr){
     let list=""
-    for(let i=0;i<myLeads.length;i++){
+    for(let i=0;i<arr.length;i++){
         list  +=`
-        <li>
-            <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]} </a>
-        </li>`
+            <li>
+                <a target='_blank' href='${arr[i]}'> ${arr[i]} </a>
+            </li>`
         
     }
     txtEl.innerHTML= list
